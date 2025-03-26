6f8a381e00d7963ccc647f34ec5128c1ccf9c226
Volodymyr <voznukvladimir781@gmail.com>
2025-03-26 23:00:23 +0100

    Создать 3Д платформу с физическим телом. Создать главного героя с блэкаутной 3Д моделью и создать физическое тело для модели. Написать скрипт в котором игрок будет двигаться по осям X, Z при нажатии кнопок WASD. Привязать к главному герою камеру от первого лица. Сделать вращение камеры по осям X, Z при движении мышки (Двигаешь мышь влево - вращение камеры влево). Ознакомится с работой со звуками в Годот

---
+++ scripts/camera_3d.gd
@@ -0,0 +1,24 @@
+extends Camera3D
+
+# camer's sensitivity
+const SENSITIVITY = 0.1
+# camer's min max angle
+const VERTICAL_LIMIT = 90
+
+# camer's vertical angle
+var vertical_angle = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
+		vertical_angle -= event.relative.y * SENSITIVITY
+		vertical_angle = clamp(vertical_angle, -VERTICAL_LIMIT, VERTICAL_LIMIT)
+		rotation_degrees.x = vertical_angle
