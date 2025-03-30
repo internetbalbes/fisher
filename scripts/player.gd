fec54b120d70d5c8ffd7625b9deb719a04ca8d93
Volodymyr <voznukvladimir781@gmail.com>
2025-03-30 15:59:04 +0200

    Fix error Add function rod

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,16 @@ const JUMP_VELOCITY = 4.5
 # camer's sensitivity
 const SENSITIVITY = 0.1
 const FISHCATCHED_MAX = 10
+ # siła grawitacji
+const gravity = Vector3(0, -9.8, 0) 
+# poziom wody
+const water_level = -5.0
+# Długość linki
+const ROD_COORD_LENGTH: float = 20.0
+# angle catching rod
+const ROD_THROW_ANGLE: float = 45.0
+# power catching rod 
+var ROD_THROW_FORCE: float = 100.0
 
 @export var label_tip: Label
 @export var label_fishcatched: Label
@@ -12,8 +22,12 @@ const FISHCATCHED_MAX = 10
 @export var timer_fishcatched: Timer
 @export var image_fishcatched: TextureRect
 @export var dialog_gameend: ConfirmationDialog
-@export var rodfloat: StaticBody3D
-@export var rodfloat_mesh: MeshInstance3D
+@export var image_pointcatch: TextureRect
+@export var rod: Node3D
+@export var rod_shaft: MeshInstance3D
+@export var rod_cord: MeshInstance3D
+@export var rod_float: StaticBody3D
+@export var rod_float_mesh: MeshInstance3D
 
 # player's state
 enum playerstate {
@@ -23,27 +37,38 @@ enum playerstate {
 	FINDLAKE,	# state finding lake
 	CASTFISHRODON,# state cast a fishing rod on
 	CASTFISHRODOFF,# state cast a fishing rod off
+	CASTFISHRODON_PROCESS,# state process cast a fishing rod on
+	CASTFISHRODOFF_PROCESS,# state process cast a fishing rod off
 }
 # player's initial state
 var state = playerstate.IDLE
 # helping for user
 var tipson: bool = false
+# vector direction catching rod
+var rod_direction : Vector3 = Vector3.ZERO
+# point cross float and water
+var rod_water_point_cross: Vector3 = Vector3.ZERO
+# player is near lake
+var near_lake: bool = false
 
 func _ready() -> void:
 	var config = ConfigFile.new()
 	if config.load("res://settings.cfg") == OK:
 		label_tip.visible = config.get_value("common", "tip", 0)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	rodfloat.visible = false
+	image_pointcatch.visible = false
+	rod.visible = false	
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if  state == playerstate.IDLE &&  event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			if rodfloat.visible:
-				state = playerstate.CASTFISHRODOFF
-			else:
+			if rod_float.position == Vector3(0, 1, 0):			
+				image_pointcatch.visible = true
 				ray_camera.enabled = true
-				state = playerstate.FINDLAKE			
+				state = playerstate.FINDLAKE
+			else:
+				timer_fishcatched.stop()
+				state = playerstate.CASTFISHRODOFF_PROCESS	
 	else: if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * SENSITIVITY))
 	else: if Input.is_action_pressed("ui_cancel"):
@@ -55,23 +80,39 @@ func _process(delta):
 		if ray_camera.is_colliding():
 			gametips(0)		
 			timer_fishcatched.one_shot = true
-			rodfloat.global_transform.origin = ray_camera.get_collision_point()
-			state = playerstate.CASTFISHRODON
+			rod_water_point_cross = ray_camera.get_collision_point()
+			image_pointcatch.visible = false
+			rod_float.position = Vector3(0, 1, 0)
+			rod_float_mesh.mesh.material.albedo_color = Color(0, 1, 0)
+			rod.rotation = Vector3(-deg_to_rad(0), 0, 0)
+			state = playerstate.CASTFISHRODON_PROCESS
 		else:
 			state = playerstate.IDLE
-		ray_camera.enabled = false
-	if state == playerstate.CASTFISHRODON:
+		ray_camera.enabled = false		
+	elif state == playerstate.CASTFISHRODON_PROCESS:
+		if rad_to_deg(-rod.rotation.x) < ROD_THROW_ANGLE:
+			rod.rotation += Vector3(-deg_to_rad(ROD_THROW_FORCE * delta), 0, 0)
+		else:
+			rod_float.global_transform.origin = rod_water_point_cross
+			state = playerstate.CASTFISHRODON
+	elif state == playerstate.CASTFISHRODON:
 		timer_fishcatched.wait_time = randf_range(5, 10)
 		timer_fishcatched.start()
-		rodfloat_mesh.mesh.material.albedo_color = Color(0, 1, 0)  # Ustawienie koloru na zielony        
-		rodfloat.visible = true
 		state = playerstate.IDLE
-	elif state == playerstate.CASTFISHRODOFF:
-		timer_fishcatched.stop()
-		if rodfloat_mesh.mesh.material.albedo_color == Color(1, 0, 0):
+	elif state == playerstate.CASTFISHRODOFF_PROCESS:
+		rod_float.position = Vector3(0, 1, 0)
+		if rad_to_deg(-rod.rotation.x) > 0:
+			rod.rotation -= Vector3(-deg_to_rad(ROD_THROW_FORCE * delta), 0, 0)
+		else:
+			state = playerstate.CASTFISHRODOFF
+	elif state == playerstate.CASTFISHRODOFF:		
+		#Player is still near water
+		if rod_float_mesh.mesh.material.albedo_color == Color(1, 0, 0):
 			# fish is catched
 			label_fishcatched.text = str(label_fishcatched.text.to_int() + 1)
-		rodfloat.visible = false
+		if near_lake:
+			gametips(1)
+			image_pointcatch.visible = true		
 		state = playerstate.IDLE
 		var count = label_fishcatched.text.to_int()
 		if  abs(count - FISHCATCHED_MAX) < 0.001:
@@ -113,7 +154,7 @@ func gametips(number: int):
 				label_tip.text = ""
 		
 func _on_timer_fishcatched_timeout() -> void:
-	rodfloat_mesh.mesh.material.albedo_color = Color(1, 0, 0)  # Czerwony kolor (RGB)
+	rod_float_mesh.mesh.material.albedo_color = Color(1, 0, 0)  # Czerwony kolor (RGB)
 
 func _on_confirmation_dialog_confirmed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")  # Przejdź do menu opcji	
@@ -124,12 +165,18 @@ func _on_confirmation_dialog_canceled() -> void:
 
 func _on_area_tip_body_entered(body: Node3D) -> void:
 	if body.name =="lake":
+		near_lake = true
 		gametips(1)
+		image_pointcatch.visible = true
+		rod.visible = true
 
 func _on_area_tip_body_exited(body: Node3D) -> void:
 	if body.name =="lake":
+		near_lake = false
 		gametips(0)
-		if rodfloat.visible:
-			state = playerstate.CASTFISHRODOFF
-			rodfloat_mesh.mesh.material.albedo_color = Color(0, 1, 0)  # Ustawienie koloru na zielony 
-		label_fishcatched.text = "0"
+		if rod_float.position == Vector3(0, 1, 0):
+			image_pointcatch.visible = false
+			rod.visible = false
+		else:	
+			timer_fishcatched.stop()
+			state = playerstate.CASTFISHRODOFF_PROCESS
